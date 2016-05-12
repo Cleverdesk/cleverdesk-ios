84c25cd0d5bdf393f3722895926d688d5d5e6d33
Jonas Franz <info@jonasfranz.software>
2016-05-12 18:07:06 +0200

    Initial Commit

---
+++ Cleverdesk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cleverdesk
+//
+//  Created by Jonas Franz on 12.05.16.
+//  Copyright © 2016 Cleverdesk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
